5603fa6814a5dee989f88da464a74225ed443c79
Robert Schrenk <robert@schrenk.cc>
2020-02-10 14:11:24 +0100

    Add dbfile for backupsize.

--- db/upgrade.php
+++ db/upgrade.php
@@ -53,7 +53,15 @@ function xmldb_local_lpfmigrator_upgrade($oldversion) {
     }
     if ($oldversion < 2020020100) {
         $table = new xmldb_table('local_lpfmigrator_instances');
-        $field = new xmldb_field('datasize', XMLDB_TYPE_INT, '10', null, null, null, null, 'adminusers');
+        $field = new xmldb_field('datasize', XMLDB_TYPE_INT, '20', null, null, null, null, 'adminusers');
+
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+    }
+    if ($oldversion < 2020021000) {
+        $table = new xmldb_table('local_lpfmigrator_instances');
+        $field = new xmldb_field('backupsize', XMLDB_TYPE_INT, '20', null, null, null, null, 'datasize');
 
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
--- version.php
+++ version.php
@@ -23,10 +23,10 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2020020101;
+$plugin->version  = 2020021000;
 $plugin->requires = 2014051200;
 $plugin->component = 'local_lpfmigrator';
-$plugin->release = '0.9 (Build: 2020020101)';
+$plugin->release = '0.9 (Build: 2020021000)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'block_eduvidual' => 2020010100, // Requires the database from moodle-block_eduvidual
