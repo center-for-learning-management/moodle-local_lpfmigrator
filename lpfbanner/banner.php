aa9136be215e357255bf9884ac49e41789cea39f
Robert Schrenk <robert@schrenk.cc>
2020-06-30 13:45:15 +0200

    new banner text.

--- lpfbanner/banner.php
+++ lpfbanner/banner.php
@@ -6,10 +6,9 @@
     </head>
     <body style="padding: 0; margin: 0;">
         <div id="usernotifybanner" style="background-color: darkred; color: white;text-align: center; padding: 20px;">
-            Achtung! Am 11. Juli wird diese Moodle-Seite deaktiviert.
-            Als Nachfolgeprojekt wird die schulübergreifende Moodle-Lernplattform unter <a href="https://www.eduvidual.at" target="_blank" style="color: lightgray;font-weight: bold; ">www.eduvidual.at</a> aufgebaut.
-            Jede Lehrperson einer Schule kann die Schule dort <a href="https://www.eduvidual.at/blocks/eduvidual/pages/register.php" target="_blank" style="color: lightgray;font-weight: bold; ">registrieren</a>.
-            Mehr Informationen auf unserer <a href="https://www.eduvidual.at/local/lpfmigrator/info.php" target="_blank" style="color: lightgray;font-weight: bold; ">Infoseite</a>!
+            Achtung! Diese Moodle-Seite wird bald auf die neue Moodle-Plattform <a href="https://www.eduvidual.at/local/lpfmigrator/info.php" target="_blank" style="color: lightgray;font-weight: bold;">eduvidual.at</a> umgestellt!
+            Ab 11. Juli werden alle Kurse gesichert und stehen im Anschluss, voraussichtlich ab 22. Juli, in der neuen Moodle-Plattform für eine Wiederherstellung zur Verfügung.
+            Weitere Informationen finden Sie unter <a href="https://www.eduvidual.at/local/lpfmigrator/info.php" target="_blank" style="color: lightgray;font-weight: bold; ">Infoseite</a>!
         </div>
     </body>
 </html>
