d25df60c0555fcd96be2b762378b08363baec7cf
Robert Schrenk <robert@schrenk.cc>
2020-06-30 13:51:32 +0200

    correct typo.

--- lpfbanner/banner.php
+++ lpfbanner/banner.php
@@ -8,7 +8,7 @@
         <div id="usernotifybanner" style="background-color: darkred; color: white;text-align: center; padding: 20px;">
             Achtung! Diese Moodle-Seite wird bald auf die neue Moodle-Plattform <a href="https://www.eduvidual.at/local/lpfmigrator/info.php" target="_blank" style="color: lightgray;font-weight: bold;">eduvidual.at</a> umgestellt!
             Ab 11. Juli werden alle Kurse gesichert und stehen im Anschluss, voraussichtlich ab 22. Juli, in der neuen Moodle-Plattform für eine Wiederherstellung zur Verfügung.
-            Weitere Informationen finden Sie unter <a href="https://www.eduvidual.at/local/lpfmigrator/info.php" target="_blank" style="color: lightgray;font-weight: bold; ">Infoseite</a>!
+            Weitere Informationen finden Sie auf unserer <a href="https://www.eduvidual.at/local/lpfmigrator/info.php" target="_blank" style="color: lightgray;font-weight: bold; ">Infoseite</a>!
         </div>
     </body>
 </html>
