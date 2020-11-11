e1b5295c2c5a1bca68679327b6faa7228c752999
Robert Schrenk <robert@schrenk.cc>
2020-11-11 11:23:04 +0100

    Update banner.js.php

--- lpfbanner/banner.js.php
+++ lpfbanner/banner.js.php
@@ -26,6 +26,11 @@
  * that they have to migrate.
  */
 
+$seconds_to_cache = 60*60*24;
+$ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
+header("Expires: $ts");
+header("Pragma: cache");
+header("Cache-Control: public, max-age=$seconds_to_cache");
 header("Content-type: text/javascript");
 require('../../../config.php');
 
