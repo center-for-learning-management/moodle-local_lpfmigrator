e76181b89ec7faad0ae1f7111ab224737ed994a0
Robert Schrenk <robert@schrenk.cc>
2021-01-07 15:55:11 +0100

    set cachable

--- lpfbanner/banner.js.php
+++ lpfbanner/banner.js.php
@@ -28,11 +28,14 @@
 
 $seconds_to_cache = 60*60*24;
 $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
+
+require('../../../config.php');
+
+$PAGE->set_cacheable(true);
 header("Expires: $ts");
 header("Pragma: cache");
 header("Cache-Control: public, max-age=$seconds_to_cache");
 header("Content-type: text/javascript");
-require('../../../config.php');
 
 $instance = optional_param('instance', '', PARAM_TEXT);
 $cache = cache::make('local_lpfmigrator', 'banners');
