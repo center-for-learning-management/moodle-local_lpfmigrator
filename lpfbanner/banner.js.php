c46f61b40f1151eb662d85d9a0ceaff4488f0a7f
Robert Schrenk <robert@schrenk.cc>
2021-01-07 15:51:08 +0100

    allow page caching.

--- lpfbanner/banner.js.php
+++ lpfbanner/banner.js.php
@@ -26,6 +26,8 @@
  * that they have to migrate.
  */
 
+$PAGE->set_cacheable(true);
+
 $seconds_to_cache = 60*60*24;
 $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
 header("Expires: $ts");
